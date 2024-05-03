be65e4a1dd67dc0ccaab95bb8ed326cd284f2100
psm817 <5004jse@gmail.com>
2024-05-03 15:30:20 +0900

    Question Entity

---
+++ src/main/java/com/sbs/sbb/Question.java
@@ -0,0 +1,24 @@
+package com.sbs.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity                                                         // Question 테이블
+public class Question {
+    @Id                                                         // Primary Key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)         // auto_increment
+    private Integer id;
+
+    @Column(length = 200)                                       // VARCHAR(200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")                          // TEXT
+    private String content;
+
+    private LocalDateTime createDate;
+}
