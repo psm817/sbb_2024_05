731b430668ed4b2602bb5b72cef632d1897d11fb
psm817 <5004jse@gmail.com>
2024-05-10 15:15:43 +0900

    question_form.html에서 오류가 있을 때 내용 알림

--- src/main/java/com/sbs/sbb/Question/QuestionController.java
+++ src/main/java/com/sbs/sbb/Question/QuestionController.java
@@ -37,7 +37,10 @@ public class QuestionController {
     }
 
     @GetMapping("/create")
-    public String create() {
+    // QuestionForm 변수는 model.addAttribute 없이 바로 뷰에서 접근할 수 있다.
+    // QuestionForm questionForm 써주는 이유 : question_form.html에서 questionForm 변수가 없으면 실행이 안되기 때문에
+    // 빈 객체라도 만든다.
+    public String create(QuestionForm questionForm) {
         return "question_form";
     }
 
