239ef7a15460d5e4955e51505c3e5ae035b42c2d
psm817 <5004jse@gmail.com>
2024-05-10 14:14:44 +0900

    유효성 체크 수동

--- src/main/java/com/sbs/sbb/Question/QuestionController.java
+++ src/main/java/com/sbs/sbb/Question/QuestionController.java
@@ -40,6 +40,22 @@ public class QuestionController {
     // 저장하기 버튼 눌렀을 때 URL 매핑
     @PostMapping("/create")
     public String questionCreate(@RequestParam(value="subject") String subject, @RequestParam(value="content") String content) {
+        if(subject == null || subject.trim().length() == 0) {
+            throw new RuntimeException("subject(을)를 입력해주세요.");
+        }
+
+        if(subject.trim().length() > 200) {
+            throw new RuntimeException("subject(을)를 200자 이하로 입력해주세요.");
+        }
+
+        if(content == null || content.trim().length() == 0) {
+            throw new RuntimeException("content(을)를 입력해주세요.");
+        }
+
+        if(content.trim().length() > 20_000) {
+            throw new RuntimeException("content(을)를 20,000자 이하로 입력해주세요.");
+        }
+
         Question q = this.questionService.create(subject, content);
 
         return "redirect:/question/list";
