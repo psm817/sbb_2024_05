4c7b22acdc19b1bc8142b47b0fa31c33ced2284c
psm817 <5004jse@gmail.com>
2024-05-10 17:45:33 +0900

    페이징 구현

--- src/main/java/com/sbs/sbb/Question/QuestionRepository.java
+++ src/main/java/com/sbs/sbb/Question/QuestionRepository.java
@@ -1,8 +1,11 @@
 package com.sbs.sbb.Question;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
     Question findBySubjectAndContent(String s, String s1);
+    Page<Question> findAll(Pageable pageable);
 }
--- src/main/java/com/sbs/sbb/Question/QuestionService.java
+++ src/main/java/com/sbs/sbb/Question/QuestionService.java
@@ -2,6 +2,9 @@ package com.sbs.sbb.Question;
 
 import com.sbs.sbb.DataNotException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -32,5 +35,11 @@ public class QuestionService {
 
         this.questionRepository.save(q);
         return q;
+    }
+
+    public Page<Question> getList(int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+
+        return this.questionRepository.findAll(pageable);
     }
 }
