24634c11b071f10c34f4773de4b2297737b7b343
psm817 <5004jse@gmail.com>
2024-05-13 18:11:24 +0900

    로그인 기능 구현

--- src/main/java/com/sbs/sbb/User/UserRepository.java
+++ src/main/java/com/sbs/sbb/User/UserRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<SiteUser, Integer> {
     @Transactional
     @Modifying
     @Query(value="ALTER TABLE site_user AUTO_INCREMENT = 1", nativeQuery = true)
     void clearAutoIncrement();
+
+    Optional<SiteUser> findByusername(String username);
 }
