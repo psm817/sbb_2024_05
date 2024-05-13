cbccfc558e85e8e26fe29401e2aa206b349b4b71
psm817 <5004jse@gmail.com>
2024-05-13 14:13:08 +0900

    작성일 기준으로 역순 조회

--- src/main/java/com/sbs/sbb/Question/QuestionService.java
+++ src/main/java/com/sbs/sbb/Question/QuestionService.java
@@ -5,9 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -38,7 +40,12 @@ public class QuestionService {
     }
 
     public Page<Question> getList(int page) {
-        Pageable pageable = PageRequest.of(page, 10);
+        // 가장 최근의 작성된 질문 순서로 리스트화
+        // SELECT * FROM question ORDER BY create_date DESC 와 같은 역할
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
 
         return this.questionRepository.findAll(pageable);
     }
